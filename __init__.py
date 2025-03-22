a707772082d5b9dd3a0027bbef61899aee02eaa1
Leoauro <605586251@qq.com>
2025-03-22 13:24:04 +0800

    init 配置

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-__all__ = ['transformer', 'embedding']
+from embedding.config import EmbeddingConfig
+from embedding.embedding import Embedding
+from transformer.config import AttentionConfig
+from transformer.decoder_only import DecoderOnly
+from transformer.encoder_only import EncoderOnly
+from transformer.transformer import Transformer
